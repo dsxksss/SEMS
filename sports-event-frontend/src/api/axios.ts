3937f0f88ca473660141da745628522de02fafc1
VentCat <2546650292@qq.com>
2025-03-26 16:34:28 +0800

    代理设置

--- sports-event-frontend/src/api/axios.ts
+++ sports-event-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:25466/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
